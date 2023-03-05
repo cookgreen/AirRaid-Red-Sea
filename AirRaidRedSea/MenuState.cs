601cadf26c21ac0e5bc215e0807433377b0f4143
DoDoCat <dell20091234@gmail.com>
2023-03-05 08:31:56 +0800

    Use MogreBites

--- AirRaidRedSea/MenuState.cs
+++ AirRaidRedSea/MenuState.cs
@@ -1,4 +1,7 @@
-﻿using org.ogre.framework;
+﻿using Mogre;
+using Mogre_Procedural.MogreBites;
+using MOIS;
+using org.ogre.framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,14 +12,125 @@ namespace AirRaidRedSea
 {
     public class MenuState : AppState
     {
+        private bool isQuit;
+
         public override void Enter()
         {
-            
+            FontManager.Singleton.GetByName("SdkTrays/Caption").Load();
+
+            sceneMgr = OgreFramework.Instance.root.CreateSceneManager(Mogre.SceneType.ST_GENERIC);
+            sceneMgr.AmbientLight = new ColourValue(0.7f, 0.7f, 0.7f);
+
+            camera = sceneMgr.CreateCamera("MenuCamera");
+            camera.SetPosition(0, 0, 800);
+            camera.LookAt(0, 0, 0);
+            camera.NearClipDistance = 1;
+            camera.AspectRatio = OgreFramework.Instance.viewport.ActualWidth / 
+                OgreFramework.Instance.viewport.ActualHeight;
+            OgreFramework.Instance.viewport.Camera = camera;
+
+            OgreFramework.Instance.trayMgr.destroyAllWidgets();
+
+            createBackground("AirraidMainScreen.png");
+
+            var btnStart = OgreFramework.Instance.trayMgr.createButton(TrayLocation.TL_CENTER, "btnStart", "Start", 200, 40, 0, 0);
+            var btnOptions = OgreFramework.Instance.trayMgr.createButton(TrayLocation.TL_CENTER, "btnOptions", "Options", 200, 40, 0, 0);
+            var btnExit = OgreFramework.Instance.trayMgr.createButton(TrayLocation.TL_CENTER, "btnExit", "Exit", 200, 40, 0, 0);
+
+            btnStart.UpMaterial = "AirRaidRedSea/UI/Button/Up";
+            btnStart.DownMaterial = "AirRaidRedSea/UI/Button/Down";
+            btnStart.OverMaterial = "AirRaidRedSea/UI/Button/Over";
+            btnStart.FontColor = new ColourValue(1, 1, 1);
+
+            btnOptions.UpMaterial = "AirRaidRedSea/UI/Button/Up";
+            btnOptions.DownMaterial = "AirRaidRedSea/UI/Button/Down";
+            btnOptions.OverMaterial = "AirRaidRedSea/UI/Button/Over";
+            btnOptions.FontColor = new ColourValue(1, 1, 1);
+
+            btnExit.UpMaterial = "AirRaidRedSea/UI/Button/Up";
+            btnExit.DownMaterial = "AirRaidRedSea/UI/Button/Down";
+            btnExit.OverMaterial = "AirRaidRedSea/UI/Button/Over";
+            btnExit.FontColor = new ColourValue(1, 1, 1);
+
+            OgreFramework.Instance.mouse.MouseMoved += mouseMoved;
+            OgreFramework.Instance.mouse.MousePressed += mousePressed;
+            OgreFramework.Instance.mouse.MouseReleased += mouseReleased;
+            OgreFramework.Instance.keyboard.KeyPressed += keyPressed;
+            OgreFramework.Instance.keyboard.KeyReleased += keyReleased;
+        }
+
+        private void createBackground(string textureName)
+        {
+            MaterialPtr material = MaterialManager.Singleton.Create("Background", "General");
+            material.GetTechnique(0).GetPass(0).CreateTextureUnitState(textureName);
+            material.GetTechnique(0).GetPass(0).DepthCheckEnabled = false;
+            material.GetTechnique(0).GetPass(0).DepthWriteEnabled = false;
+            material.GetTechnique(0).GetPass(0).LightingEnabled = false;
+
+            Rectangle2D rect = new Rectangle2D(true);
+            rect.SetCorners(-1.0f, 1.0f, 1.0f, -1.0f);
+            rect.SetMaterial("Background");
+
+            rect.RenderQueueGroup = (byte)RenderQueueGroupID.RENDER_QUEUE_BACKGROUND;
+
+            AxisAlignedBox aab = new AxisAlignedBox();
+            aab.SetInfinite();
+            rect.BoundingBox = aab;
+
+            SceneNode node = sceneMgr.RootSceneNode.CreateChildSceneNode("Background");
+            node.AttachObject(rect);
         }
 
         public override void Exit()
         {
 
+        }
+
+        public override void Update(double timeSinceLastFrame)
+        {
+            if(isQuit)
+            {
+                shutdown();
+                return;
+            }
+        }
+
+        public override void buttonHit(Button button)
+        {
+            base.buttonHit(button);
+        }
+
+        public bool keyPressed(KeyEvent keyEventRef)
+        {
+            if (OgreFramework.Instance.keyboard.IsKeyDown(MOIS.KeyCode.KC_ESCAPE))
+            {
+                isQuit = true;
+                return true;
+            }
+
+            OgreFramework.Instance.KeyPressed(keyEventRef);
+            return true;
+        }
+        public bool keyReleased(KeyEvent keyEventRef)
+        {
+            OgreFramework.Instance.KeyReleased(keyEventRef);
+            return true;
+        }
+
+        public bool mouseMoved(MouseEvent evt)
+        {
+            if (OgreFramework.Instance.trayMgr.injectMouseMove(evt)) return true;
+            return true;
+        }
+        public bool mousePressed(MouseEvent evt, MouseButtonID id)
+        {
+            if (OgreFramework.Instance.trayMgr.injectMouseDown(evt, id)) return true;
+            return true;
+        }
+        public bool mouseReleased(MouseEvent evt, MouseButtonID id)
+        {
+            if (OgreFramework.Instance.trayMgr.injectMouseUp(evt, id)) return true;
+            return true;
         }
     }
 }
---
+++ AirRaidRedSea/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Air Riad : Red Sea")]
+[assembly: AssemblyDescription("Main Executable for Air Raid : Red Sea")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Yin Xiaoguang")]
+[assembly: AssemblyProduct("Air Raid : Red Sea")]
+[assembly: AssemblyCopyright("GNU GPLv3")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("801dc335-56b4-4415-93a6-ade84ff1d4a3")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
