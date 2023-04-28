89f4db0e5b0a7387390635ae380b915cec9acef1
DoDoCat <dell20091234@gmail.com>
2023-04-28 13:50:31 +0800

    Update Project

--- AirRaidLevelEditor/Program.cs
+++ AirRaidLevelEditor/Program.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace AirRaidLevelEditor
 {
 	public class Program
 	{
-
+		public static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.Run(new frmMain());
+		}
 	}
 }
